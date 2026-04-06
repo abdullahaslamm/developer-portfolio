ad6c613dd8281c913fd34109f7acae0459f1367f
abdullahaslamm <abdullahaslamm2001@gmail.com>
2026-04-07 02:58:18 +0500

    Remove time

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -38,11 +38,6 @@ export default function Footer() {
               Anna Turner
             </Link>
           </p>
-          <hr className="hidden h-6 border-l border-muted md:flex" />
-          <span className="flex hidden flex-row items-center space-x-2 md:flex">
-            <p className="text-xs text-muted-foreground">Local time:</p>
-            <p className="text-sm font-semibold">{time} UTC+1</p>
-          </span>
         </span>
         <Link
           href="https://discord.com/users/428013748847116289"
